16161fe63653ae38bbd18ebdcc40249d25d2d6e6
Cameron <cameron.p.buckingham@gmail.com>
2014-07-28 08:36:39 -0600

    did all fruits

--- fruits.rb
+++ fruits.rb
@@ -1,1 +1,21 @@
 fruits = ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes']
+
+
+p fruits.length
+
+p fruits.index('bananas')
+
+p fruits.insert(1, 'rasberries')
+
+fruits.each do |fruit|
+p fruit.length
+end
+
+p fruits.map(&:capitalize)
+
+fruits.each do |fruit|
+  if fruit.include?('g')
+    p fruit
+  end
+end
+
